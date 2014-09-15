43e48c10139ee6d2d96af0fe25794c2ed9176614
praveen <praveen.beatle@gmail.com>
2014-09-15 12:17:36 +0530

    following/followers view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,13 +50,11 @@ class UsersController < ApplicationController
   def following 
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
-    render 'show_follow'
   end
 
   def followers 
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
   end
 
   def destroy
